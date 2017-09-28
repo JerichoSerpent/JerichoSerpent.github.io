224bb677ff135d54bb8b051796dfe7d8b5ce982a
Jozef <jozefw@sbcglobal.net>
2017-09-28 10:46:09 -0700

    final commit before production

--- js/app.js
+++ js/app.js
@@ -1,34 +1,50 @@
 $(document).ready(function() {
-var pageLayout = $('#data_output');
+    var pageLayout = $('#data_output');
+
     $('.btn#userNameSubmit').on('click',
         function() {
             var gitUserName = $('#githubUserName').val(),
-            	url = 'https://api.github.com/users/';
-             	urlUserName = 'https://api.github.com/users/' + gitUserName,
-            	gitRepo = 'https://api.github.com/users/' + gitUserName + '/repos';
+                url = 'https://api.github.com/users/';
+            urlUserName = 'https://api.github.com/users/' + gitUserName,
+                gitRepo = 'https://api.github.com/users/' + gitUserName + '/repos';
 
             getUserName(urlUserName);
 
         });
 
-    function appendToPage(response){
-    	var handle = response.data.login;
-    	var followerCount = response.data.followers;
-    	var followers = response.data.followers;
-    	if(response.data.message === "Not Found"){
-    		pageLayout.html("<h1>No User Info !!!!</h1>")
-    	} else {
-    		pageLayout.html("<h1>Handle: " + handle + "<br>"+ " User Count: " +followerCount + "</h1>"+"<br><h6>" + followers +"<h6>")
-    	}
+    function appendToPage(response) {
+        console.log(response);
+        var handle = response.data.login;
+        var followerCount = response.data.followers;
+        var followers = response.data.followers_url;
+        $.get(followers, function(data, status) {
+            console.log(data);
+            data.forEach(function(item, index) {
+                var list = "<li class='hidden' >" + "<img src='" + item.avatar_url + "' alt=''/>";
+                $('.hold').append(list);
+            });
+
+            function loadMore() {
+                $(".hold .hidden").slice(0, 2).removeClass("hidden");
+            }
+
+            loadMore();
+            $("#btnLoadMore").removeClass('hidden');
+            $("#btnLoadMore").on("click", loadMore);
+        });
+        if (response.data.message === "Not Found") {
+            pageLayout.html("<h1>No User Info !!!!</h1>");
+        } else {
+            pageLayout.html("<h1>Handle: " + handle + "<br>" + " Follower Count: " + followerCount + "</h1>" + "<br>");
+        }
     }
 
-     function getUserName(url) {
+    function getUserName(url) {
         $.ajax({
             url: url,
             dataType: "jsonp",
             success: function(returnData, status) {
-            	console.log(returnData);
-            	appendToPage(returnData);
+                appendToPage(returnData);
             }
         });
     }
