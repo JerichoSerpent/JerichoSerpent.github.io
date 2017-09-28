5a540a96a60718185194984a1bd56e246673c2ec
Jozef <jozefw@sbcglobal.net>
2017-09-27 18:26:39 -0700

    add followers

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+var pageLayout = $('#data_output');
     $('.btn#userNameSubmit').on('click',
         function() {
             var gitUserName = $('#githubUserName').val(),
@@ -8,14 +8,27 @@ $(document).ready(function() {
             	gitRepo = 'https://api.github.com/users/' + gitUserName + '/repos';
 
             getUserName(urlUserName);
+
         });
+
+    function appendToPage(response){
+    	var handle = response.data.login;
+    	var followerCount = response.data.followers;
+    	var followers = response.data.followers;
+    	if(response.data.message === "Not Found"){
+    		pageLayout.html("<h1>No User Info !!!!</h1>")
+    	} else {
+    		pageLayout.html("<h1>Handle: " + handle + "<br>"+ " User Count: " +followerCount + "</h1>"+"<br><h6>" + followers +"<h6>")
+    	}
+    }
 
      function getUserName(url) {
         $.ajax({
             url: url,
             dataType: "jsonp",
             success: function(returnData, status) {
-
+            	console.log(returnData);
+            	appendToPage(returnData);
             }
         });
     }
